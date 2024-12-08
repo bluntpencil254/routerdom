48dc88a7138341cd59beb4bfd15fe16c26b24359
bluntpencil254 <makorimike9@gmail.com>
2024-12-08 14:04:02 +0300

    final project detailed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  
+
+  return (
+    <div
+    className='bg-green-600 text-gray-500'>
+      react router
+    </div>
+  )
+}
+
+export default App
